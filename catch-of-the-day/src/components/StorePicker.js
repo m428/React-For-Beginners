5fb6712ead070c45f396abbd58f51344f1ca452a
Mala Nimalasuriya <m428@users.noreply.github.com>
2018-01-07 14:41:48 -0800

    write HTML with JSX

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
-class StorePicker extends React.Component { // basic component, must have a method
+class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    return (
+    <form className="store-selector">
+      { /* this is a JSX comment */}
+      <h2>Please Enter a Store</h2>
+      <input type="text" required placeholder="Store Name" />
+      <button type="submit">Visit Store -></button>
+    </form>
+    )
   }
 }
 
