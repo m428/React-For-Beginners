da342a47ed9ca7717f6e0efe237265b692f569d6
Mala Nimalasuriya <m428@users.noreply.github.com>
2018-01-07 14:48:41 -0800

    import css files

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'; // load React library in React variable
 import { render } from 'react-dom';
 
+import './css/style.css'; // import css form style.css
+
 import StorePicker from './components/StorePicker';
 
 render(<StorePicker/>, document.querySelector('#main')) // render StorePicker component in #main
