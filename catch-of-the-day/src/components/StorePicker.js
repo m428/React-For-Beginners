3c78e0a62c7bf77f6d246a9927303f0c1bac6cc2
Mala Nimalasuriya <m428@users.noreply.github.com>
2018-01-07 14:08:02 -0800

    add StorePicker component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StorePicker extends React.Component { // basic component, must have a method
+  render() {
+    return <p>Hello</p>
+  }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react'; // load React library in React variable
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main')) // render StorePicker component in #main
