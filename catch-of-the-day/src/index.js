ad105736a6c6364ac52f674b8b32120948f8223b
Mala Nimalasuriya <m428@users.noreply.github.com>
2018-01-07 19:19:54 -0800

    begin routing with React Router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,30 @@
 import React from 'react'; // load React library in React variable
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 
 import './css/style.css'; // import css form style.css
 
 import App from './components/App'
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound'
 
-render(<App/>, document.querySelector('#main')) // render StorePicker component in #main
+
+// Root componet is the router
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match exactly pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main')) // render StorePicker component in #main
+
+// if on store page show App component
+// if on homepage show StorePicker componet
+// if url wrong show url not found component
